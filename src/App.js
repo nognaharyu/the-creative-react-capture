f2caf6807cf58951651b7dd50eb7aa9133e4d7e6
nognaharyu <nognaharyu@gmail.com>
2020-12-17 22:35:21 +0900

    2.AboutSection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <h1>film</h1>
+      <AboutUs/>
     </div>
   );
 }
