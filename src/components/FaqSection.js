76e415c2bf9a3b3f836db96a1cfde4c574963dae
nognaharyu <nognaharyu@gmail.com>
2020-12-20 18:38:23 +0900

    4.Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from "react"
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start</h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>
+                    Sapiente commodi earum ad ullam officiis, neque perferendis, ipsam soluta laborum eius libero provident, 
+                    veniam mollitia facere blanditiis illo. Quia, doloremque maxime!
+                </p>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>
+                    Sapiente commodi earum ad ullam officiis, neque perferendis, ipsam soluta laborum eius libero provident, 
+                    veniam mollitia facere blanditiis illo. Quia, doloremque maxime!
+                </p>
+            </div>
+            <div className="question">
+                <h4>Differnt Payment Methods</h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>
+                    Sapiente commodi earum ad ullam officiis, neque perferendis, ipsam soluta laborum eius libero provident, 
+                    veniam mollitia facere blanditiis illo. Quia, doloremque maxime!
+                </p>
+            </div>
+            <div className="question">
+                <h4>What Products do you offer</h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>
+                    Sapiente commodi earum ad ullam officiis, neque perferendis, ipsam soluta laborum eius libero provident, 
+                    veniam mollitia facere blanditiis illo. Quia, doloremque maxime!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ServiceSection from "../components/ServiceSection";
+import FaqSection from "../components/FaqSection"
 const AboutUs = () =>{
     return(
     <>
         <AboutSection/>
         <ServiceSection/>
+        <FaqSection/>
     </>
     );
 }
